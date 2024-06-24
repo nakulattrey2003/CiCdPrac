f17e17d102cb63400d820ffd66f32b615dc65dd4
Nakul Attrey <nakulattrey@gmail.com>
2024-06-24 08:51:06 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello Nakul");
+})
+
+app.listen(5000, (req, res) => {
+    console.log('Port started on port 5000');
+})
